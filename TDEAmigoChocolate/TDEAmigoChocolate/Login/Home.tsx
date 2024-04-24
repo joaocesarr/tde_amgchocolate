2b1696da3ec422d6c7e4fa4a6aea67618b12c6d2
Joao Cesar <127664429+joaocesarr@users.noreply.github.com>
2024-04-24 00:04:19 -0300

    Update Home.tsx

--- TDEAmigoChocolate/TDEAmigoChocolate/Login/Home.tsx
+++ TDEAmigoChocolate/TDEAmigoChocolate/Login/Home.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
+  const handleStartPress = () => {
+    // Coloque aqui a lógica para iniciar a ação desejada
+    console.log("Ação de iniciar pressionada");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Amigo Chocolate</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Lista de Amigos')}
-      >
+      <TouchableOpacity style={styles.button} onPress={handleStartPress}>
         <Text style={styles.buttonText}>Iniciar</Text>
       </TouchableOpacity>
     </View>
@@ -18,24 +20,24 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff', 
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
   },
   title: {
     fontSize: 32,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   button: {
-    backgroundColor: '#ff6f61',
+    backgroundColor: "#ff6f61",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5,
   },
   buttonText: {
     fontSize: 18,
-    color: 'white',
+    color: "white",
   },
 });
 
